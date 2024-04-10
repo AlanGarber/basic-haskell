fc77c30df5ca6307643e100b59d8b93b7b27cbec
Alan Garber <alangarber1@gmail.com>
2024-04-10 14:42:05 -0300

    finished 'continuando'

--- TP/TP1-SueldosDocentes.hs
+++ TP/TP1-SueldosDocentes.hs
@@ -1,4 +1,3 @@
-import GHC.Platform.Host (hostPlatformArchOS)
 {-
     Calcular el sueldo de un docente universitario de UTN, dado su cargo, cantidad de horas que trabaja, y su antigüedad en años.
     El básico depende del cargo que tenga la persona, también tiene un adicional por antigüedad y luego varía proporcionalmente dependiendo de la cantidad de horas que trabaje.
@@ -46,3 +45,48 @@ multiplicadorHoras horas
 
 sueldoDocente :: String -> Int -> Int -> Float
 sueldoDocente cargo horas antiguedad = sueldoBase cargo * antiguedadPorcentaje antiguedad * multiplicadorHoras horas
+
+{-
+    Averiguar a qué distancia de la línea de pobreza está un docente universitario
+
+    Se conocen los costos de la canasta básica total, que es la que define la línea de pobreza, para familias de diferente composición, a noviembre 2023.
+    1 integrante (adulto) 126000
+    3 integrantes (2 adultos y 1 adulto mayor) 310000
+    4 integrantes (2 adultos y 2 menores) 390000
+    5 integrantes (2 adultos y 3 menores) 410000 
+
+    Se cuenta con una función que calcula el salario docente a la misma fecha. (Una forma posibles de calcularlo, en este otro ejercicio)
+
+    También se concen los porcentajes de aumento, tanto de la inflación como del salario, de  diciembre 2023 a febrero 2024, ambos inclusive:
+    Inflación General (IPC): 71%
+    Aumento salarial: 22%
+    (Datos oficiales. El aumento salarial fue 6% en diciembre y 10% en febrero según acuerdo paritario anterior. Por decisión del estado nacional sin acuerdo partitario, en febrero se agrega 6% no acumulativo)
+
+    Requerimiento:
+    Obtener la diferencia entre los ingresos del docente y el costo de la canasta básica, que puede ser un valor positivo o negativo. Tomar como base valores precisos de un determinado momento  (noviembre 2023) y actualizarlos según los porcentajes de variación de la inflación y salarios producidos desde entonces a la actualidad.
+
+    Ejemplo
+    Un docente que en noviembre 2023 cobraba $348000, con una familia de 3 integrantes, teniendo en cuenta incrementos de 71% y 22%, en febrero 2024 está  $105540 por debajo de la línea de pobreza.
+    El mismo docente, haciendo el calculo para el mismo noviembre (o sea, con ambos incrementos en 0%) estaba $38000 por sobre la linea de pobreza.
+    Cuando estén disponibles los indices de inflación de marzo y el aumento salarial, en caso de haber, se podria volver a hacer el cálculo, a ver si la situación mejoró o empeoró.
+-}
+
+actualizarSueldo :: Float -> Float -> Float -> Float
+actualizarSueldo sueldo inflacion aumento = sueldo * (1 + aumento)
+
+
+inflacionFebrero = 0.71
+aumentoFebrero = 0.22
+
+actualizarCanastaBasica :: Int -> Float -> Float
+actualizarCanastaBasica integrantes inflacion 
+    | integrantes == 1 = 126000 * (1 + inflacion)
+    | integrantes == 3 = 310000 * (1 + inflacion)
+    | integrantes == 4 = 390000 * (1 + inflacion)
+    | integrantes == 5 = 410000 * (1 + inflacion)
+    | otherwise = 0
+
+diferenciaCanastaBasicaActualizada :: String -> Int -> Int -> Int -> String -> Float
+diferenciaCanastaBasicaActualizada cargo horas antiguedad integrantes mes 
+    | mes == "noviembre" =  sueldoDocente cargo horas antiguedad  - actualizarCanastaBasica integrantes 0
+    | mes == "febrero" =  actualizarSueldo  (sueldoDocente cargo horas antiguedad) inflacionFebrero aumentoFebrero   - actualizarCanastaBasica integrantes inflacionFebrero
